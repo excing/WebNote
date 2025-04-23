60936fb5a1edd67b784f8c61015a556cbcc789ef
chshqiang <courage.exc@gmail.com>
2025-04-23 15:49:54 +0800

    Update: 添加 mainfest.json 和 serviceWorker

---
+++ src/service-worker.js
@@ -0,0 +1,98 @@
+/// <reference types="@sveltejs/kit" />
+import { build, files, version } from '$service-worker';
+
+// Create a unique cache name for this deployment
+// 1741496496157
+// 1741497241877
+const CACHE = `cache-${version}`;
+
+const ASSETS = [
+  ...build, // the app itself
+  ...files  // everything in `static`
+];
+
+self.addEventListener('install', (event) => {
+  console.log(`service worker version: ${version}`);
+
+  // Create a new cache and add all files to it
+  async function addFilesToCache() {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(ASSETS);
+  }
+
+  event.waitUntil(addFilesToCache());
+});
+
+self.addEventListener('activate', (event) => {
+  console.log(`service worker version: ${version}`);
+
+  // Remove previous cached data from disk
+  async function deleteOldCaches() {
+    for (const key of await caches.keys()) {
+      if (key !== CACHE) await caches.delete(key);
+    }
+  }
+
+  event.waitUntil(deleteOldCaches());
+});
+
+self.addEventListener('fetch', (event) => {
+  // 过滤掉 chrome-extension 协议的请求
+  // 当你在 Service Worker 中遇到 Uncaught (in promise) TypeError: Failed to execute 'put' on 'Cache': Request scheme 'chrome-extension' is unsupported 错误时，这通常是因为 Service Worker 尝试缓存来自 Chrome 扩展（chrome-extension 协议）的资源，但 Cache API 不支持这种协议的请求。
+  if (event.request.url.startsWith('chrome-extension:')) {
+    return;
+  }
+  // ignore POST requests etc
+  if (event.request.method !== 'GET') return;
+
+  async function respond() {
+    const url = new URL(event.request.url);
+    const cache = await caches.open(CACHE);
+
+    // `build`/`files` can always be served from the cache
+    if (ASSETS.includes(url.pathname)) {
+      const response = await cache.match(url.pathname);
+
+      if (response) {
+        return response;
+      }
+    }
+
+    // 优先从缓存返回数据
+    const response = await cache.match(event.request);
+
+    if (response) {
+      return response;
+    }
+
+    // for everything else, try the network first, but
+    // fall back to the cache if we're offline
+    try {
+      const response = await fetch(event.request);
+
+      // if we're offline, fetch can return a value that is not a Response
+      // instead of throwing - and we can't pass this non-Response to respondWith
+      if (!(response instanceof Response)) {
+        throw new Error('invalid response from fetch');
+      }
+
+      if (response.status === 200) {
+        cache.put(event.request, response.clone());
+      }
+
+      return response;
+    } catch (err) {
+      const response = await cache.match(event.request);
+
+      if (response) {
+        return response;
+      }
+
+      // if there's no cache, then just error out
+      // as there is nothing we can do to respond to this request
+      throw err;
+    }
+  }
+
+  event.respondWith(respond());
+});
